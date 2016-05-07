77ffe868b7a00597f1efdd52d0dadd325fe54631
ludai0106 <ludai0106@gmail.com>
2016-05-07 10:33:29 +0200

    update answer_dis figure

--- src/MatlabFile/answerCounts.m
+++ src/MatlabFile/answerCounts.m
@@ -1,8 +1,29 @@
 M = csvread('/Users/ludai/Desktop/Study/Github/ComplexNetwork/src/StackOverflowCrawler/QuestionCrawler/answer_statistic.csv',1,0)
 X = M(:,1)
 Y = M(:,2)
+[xData, yData] = prepareCurveData( X, Y )
 figure
-plot(X,Y)
-title('number of answers distribution')
+ax = axes()
+bar(X,Y,'y','BaseValue',0)
+set(ax, 'Xlim', [-1,14])
+set(ax, 'XTick', [-1:14])
+set(ax, 'Ylim', [0,3500])
+set(ax, 'YTick', [0:300:3500])
+title('answers distribution')
+
+hold on 
+ft = fittype( 'smoothingspline' )
+
+[fitresult, gof] = fit( xData, yData, ft );
+
+h = plot( fitresult);
+legend( h, 'questionNum vs. answerNum', 'answer-dis', 'Location', 'NorthEast' );
+% Label axes
 xlabel('number of answers')
 ylabel('number of questions')
+ hold off
+
+
+
+
+
